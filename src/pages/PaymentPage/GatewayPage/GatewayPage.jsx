b2bd6c28452b702c753717a37e70502081278c17
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-05-03 00:55:52 +0600

    Payment GateWay Page is Done

--- src/pages/PaymentPage/GatewayPage/GatewayPage.jsx
+++ src/pages/PaymentPage/GatewayPage/GatewayPage.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+
 import { IoIosArrowBack } from 'react-icons/io';
+import { FaLock, FaQuestionCircle } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const GatewayPage = () => {
+
+    const [showForm, setShowForm] = useState(false)
+
+    const handleRadioChange = (event) => {
+        const targetValue = event.target.value;
+        setShowForm(targetValue === 'different');
+    }
+
     return (
         <div className='mt-12'>
             <div className='border p-2'>
@@ -29,8 +39,9 @@ const GatewayPage = () => {
             </div>
 
             <div className='mt-16'>
-                <h1>Shipping Method</h1>
-                <div className='grid md:grid-cols-12 gap-2 border p-4 mt-4 items-center'>
+                <h1 className='text-xl'>Payment</h1>
+                <p className='text-[14px] text-[#838383]'>All transactions are secure and encrypted.</p>
+                <div className='grid grid-cols-12 gap-2 border border-b-0 p-4 mt-4 items-center'>
                     <div className='col-span-10 flex gap-2'>
                         <span><input checked className='accent-[#5b9bbe]' type="radio" name="" id="" /></span>
                         <p className=' text-[14px]'>Credit card</p>
@@ -40,6 +51,77 @@ const GatewayPage = () => {
                         <img className='w-8 ' src="https://i.ibb.co/8bPST2H/mastercard-logo-freelogovectors-net-640x400.png" alt="" />
                     </div>
                 </div>
+
+
+                <form className='border p-3 bg-base-200 flex flex-wrap md:grid  md:grid-cols-2  gap-3'>
+
+              
+                        <label className="input w-full input-bordered flex items-center gap-2 col-span-2">
+                            <input type="text" className="grow  focus:border-[#5b9bbe]" placeholder="Card Number" />
+                            <p className='text-[#888888]'><FaLock /></p>
+                        </label>
+
+                        <label className="input w-full input-bordered flex items-center gap-2 col-span-2">
+                            <input type="text" className="grow  focus:border-[#5b9bbe]" placeholder="Name On card" />
+                        </label>
+                        <label className="input w-full input-bordered flex items-center gap-2">
+                            <input type="text" className="grow  focus:border-[#5b9bbe]" placeholder="Expiration date (MM/YY)" />
+                        </label>
+                        <label className="input w-full input-bordered flex items-center gap-2 ">
+                            <input type="text" className="grow  focus:border-[#5b9bbe]" placeholder="Security code" />
+                            <p className='text-[#888888]'><FaQuestionCircle /></p>
+                        </label>
+                </form>
+
+            </div>
+            <div className='mt-16'>
+                <h1 className='text-xl'>Billing address</h1>
+                <p className='text-[14px] text-[#838383]'>Select the address that matches your card or payment method.</p>
+                <div className='grid md:grid-cols-12 gap-2 border p-4 mt-4 items-center'>
+                    <form className='col-span-12 grid gap-4'>
+                        <div className='flex gap-3'>
+                            <input
+                                type="radio"
+                                name="radio-1"
+                                className="radio checked:bg-[#5b9bbe]"
+                                value="same"
+                                checked={!showForm}
+                                onChange={handleRadioChange}
+                            />
+                            <label>Same as shipping address</label>
+                        </div>
+                        <hr />
+                        <div className='flex gap-3'>
+                            <input
+                                type="radio"
+                                name="radio-1"
+                                className="radio checked:bg-[#5b9bbe]"
+                                value="different"
+                                checked={showForm}
+                                onChange={handleRadioChange}
+                            />
+                            <label>Use a different billing address</label>
+                        </div>
+                    </form>
+                </div>
+
+                <div className={`border p-3 bg-base-200 grid gap-3 ${showForm ? '' : 'hidden'}`}>
+                    <form className="grid md:grid-cols-2 gap-3">
+                        <select className="select select-bordered w-full col-span-2">
+                            <option disabled selected>Country</option>
+                            <option>Han Solo</option>
+                            <option>Greedo</option>
+                        </select>
+                        <input placeholder="First name" className="w-full border py-3 rounded-md pl-3 focus:border-[#5b9bbe]" type="text" />
+                        <input placeholder="Last name" className="w-full border py-3 rounded-md pl-3 focus:border-[#5b9bbe]" type="text" />
+                        <input placeholder="Address" className="col-span-2 w-full border py-3 rounded-md pl-3 focus:border-[#5b9bbe]" type="text" />
+                        <input placeholder="Apartment, suite, etc." className="col-span-2 w-full border py-3 rounded-md pl-3 focus:border-[#5b9bbe]" type="text" />
+                        <input placeholder="City" className="w-full border py-3 rounded-md pl-3 focus:border-[#5b9bbe]" type="text" />
+                        <input placeholder="Postal code" className="w-full border py-3 rounded-md pl-3 focus:border-[#5b9bbe]" type="text" />
+                        <input placeholder="+880" className="col-span-2 w-full border py-3 rounded-md pl-3 focus:border-[#5b9bbe]" type="text" />
+                    </form>
+                </div>
+
             </div>
             <div className="col-span-2 md:mb-56 mt-6 flex md:justify-between md:items-center gap-5 md:mt-12">
                 <a className=" flex text-[14px] items-center text-[#5b9bbe]" href=""><span className="text-[18px]"> <IoIosArrowBack /></span> Return to bag</a>
